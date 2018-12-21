f1c10a1386078158f5429ad686bc2b64ec8caadc
Damien Lespiau <damien@weave.works>
2018-12-21 11:08:54 +0000

    apigen: Import pulumi api gen
    This starting state is from https://github.com/pulumi/pulumi-kubernetes, only
    keeping the typescript generation (ie. stripping the python part away).

---
+++ cmd/apigen/main.go
@@ -0,0 +1,93 @@
+// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2018, The jk Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/jkcfg/kubernetes/pkg/gen"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		log.Fatal("Usage: gen <swagger-file> <template-dir> <out-dir>")
+	}
+
+	swagger, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	data := map[string]interface{}{}
+	err = json.Unmarshal(swagger, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	templateDir := os.Args[2]
+	outdir := fmt.Sprintf("%s", os.Args[3])
+
+	writeNodeJSClient(data, outdir, templateDir)
+}
+
+func writeNodeJSClient(data map[string]interface{}, outdir, templateDir string) {
+	inputAPIts, ouputAPIts, providerts, helmts, packagejson, err := gen.NodeJSClient(data, templateDir)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.MkdirAll(outdir, 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	typesDir := fmt.Sprintf("%s/types", outdir)
+	err = os.MkdirAll(typesDir, 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/input.ts", typesDir), []byte(inputAPIts), 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/output.ts", typesDir), []byte(ouputAPIts), 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/provider.ts", outdir), []byte(providerts), 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/helm.ts", outdir), []byte(helmts), 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/package.json", outdir), []byte(packagejson), 0777)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s/package.json\n", outdir)
+	fmt.Println(err)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jkcfg/kubernetes
+
+require (
+	github.com/ahmetb/go-linq v3.0.0+incompatible
+	github.com/cbroglie/mustache v1.0.1
+	github.com/gogo/protobuf v1.2.0 // indirect
+	github.com/jinzhu/copier v0.0.0-20180308034124-7e38e58719c3
+	github.com/mitchellh/go-wordwrap v1.0.0
+	k8s.io/apimachinery v0.0.0-20181220065808-98853ca904e8
+)
