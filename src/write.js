28c327d12e4c79c5945d416bdaec0105fe876df5
Michael Bridgen <mikeb@squaremobius.net>
2019-01-10 17:41:34 +0000

    Add util for writing a list of resources out
    It's handy to be able to pass the result of e.g., evaluating an
    overlay, to a procedure which will write it out to
    files. writeResources takes the writer and will write the resources
    out one to a file -- each named for the name and kind of the resource
    and put in a directory named for its namespace.

---
+++ src/write.js
@@ -0,0 +1,16 @@
+
+// Write all the individual resources out in directories and files
+// according to their namespace, kind and name.
+const writeResources = write => function(resources) {
+  resources.forEach((r) => {
+    const filename = `${r.metadata.name}-${r.kind.toLowerCase()}.yaml`;
+    let path = filename;
+    if (r.metadata.namespace) {
+      path = `${r.metadata.namespace}/${filename}`;
+    }
+    console.log("write ", r, path);
+    write(r, path);
+  });
+};
+
+export { writeResources };
