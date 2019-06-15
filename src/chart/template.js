3b97fc3e0f20e593b21af190ad689f7250643ef7
Michael Bridgen <mikeb@squaremobius.net>
2019-06-15 17:27:43 +0100

    Depend on handlebars in test rather than lib
    Rather than having handlebars as a dependency, parameterise the
    template module procedures on the `compile`r, and supply handlebars
    specifically in the tests.

--- src/chart/template.js
+++ src/chart/template.js
@@ -1,20 +1,19 @@
-import handlebars from 'handlebars/lib/handlebars';
-
 const isTemplateFile = info => (!info.isDir && info.name.endsWith('.yaml'));
 
-// readString x filename -> Promise (values -> resource)
-async function loadTemplate(readString, path) {
+// { readString, compile } -> filename -> Promise (values -> resource)
+const loadTemplate = ({ readString, compile }) => async function load(path) {
   const file = await readString(path);
-  const template = handlebars.compile(file);
+  const template = compile(file);
   return values => template({ values });
-}
-
-// { readString, dir } -> values -> Promise [string]
-const load = ({ readString, dir }) => async function resources(values) {
-  const d = dir('templates');
-  const loadTempl = info => loadTemplate(readString, info.path);
-  const templates = await Promise.all(d.files.filter(isTemplateFile).map(loadTempl));
-  return templates.map(t => t(values));
 };
 
-export { load };
+// { readString, compile, dir } -> values -> Promise [string]
+const loadDir = ({ readString, compile, dir }) => async function templates(values) {
+  const load = loadTemplate({ readString, compile });
+  const d = dir('templates');
+  const loadTempl = info => load(info.path);
+  const allTempl = await Promise.all(d.files.filter(isTemplateFile).map(loadTempl));
+  return allTempl.map(t => t(values));
+};
+
+export { loadDir, loadTemplate };
