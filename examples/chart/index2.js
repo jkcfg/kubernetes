c592815e63558458d00865870b1d47777ec00a2d
Michael Bridgen <mikeb@squaremobius.net>
2019-06-16 09:50:47 +0100

    Adjust example to use handlebars itself

--- examples/chart/index2.js
+++ examples/chart/index2.js
@@ -1,10 +1,11 @@
 // Example of a Helm chart analogue, using handlebars
 
 import { chart } from '@jkcfg/kubernetes/chart';
-import { load } from '@jkcfg/kubernetes/chart/template';
+import { loadDir } from '@jkcfg/kubernetes/chart/template';
 import { writeYAMLStream } from '@jkcfg/kubernetes/write';
 import * as resource from '@jkcfg/std/resource';
 import std from '@jkcfg/std';
+import handlebars from 'handlebars/lib/handlebars';
 
 const defaults = {
   name: 'helloworld',
@@ -16,7 +17,7 @@ const defaults = {
 };
 
 const readString = path => resource.read(path, { encoding: std.Encoding.String });
-const templates = load({ readString, ...resource });
+const templates = loadDir({ readString, compile: handlebars.compile, ...resource });
 
 // Because we get strings out of the templates, rather than objects,
 // we don't need to serialise them; so, use writeYAMLStream, which we
