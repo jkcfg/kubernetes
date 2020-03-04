cacca66f5ca2845004145b8a3686e2e064bc7aff
Michael Bridgen <mikeb@squaremobius.net>
2020-03-04 19:52:08 +0000

    Let people get all the files in one dir
    `valuesForGenerate` will put each file in a directory named for its
    namespace, if it has one. But sometimes you just want them all in the
    same dir (and collisions be damned).

--- src/generate.js
+++ src/generate.js
@@ -1,12 +1,12 @@
 // Given an array (or a promise of an array) of Kubernetes resources,
 // return a list of values suitable for use with `jk generate`
 async function valuesForGenerate(resources, opts = {}) {
-  const { prefix = '' } = opts;
+  const { prefix = '', namespaceDirs = true } = opts;
   const all = await Promise.resolve(resources);
   return all.map((r) => {
     const filename = `${r.metadata.name}-${r.kind.toLowerCase()}.yaml`;
     let path = filename;
-    if (r.metadata.namespace) {
+    if (namespaceDirs && r.metadata.namespace) {
       path = `${r.metadata.namespace}/${filename}`;
     }
     if (prefix !== '') {
